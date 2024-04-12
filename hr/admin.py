3290e8bddfd495d461706c98122e074216ac9782
jacob@appyventures.com <jacob@appyventures.com>
2024-04-12 21:28:04 +0300

    first commit

---
+++ hr/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+
+from hr.models import (
+    Department,
+    Employee,
+    Position,
+)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent_department',)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'department', 'is_manager')
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.save()
+        except ValidationError as e:
+            form.add_error(None, e)
+            super().save_model(request, obj, form, change)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('username', 'position', 'hire_date',)
---
+++ hr/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+    parent_department = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    title = models.CharField(max_length=200)
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    is_manager = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.is_manager:
+            existing_manager = Position.objects.filter(department=self.department, is_manager=True).exists()
+            if existing_manager:
+                raise ValidationError(f"Manager already exists in the {self.department.name} department.")
+        super(Position, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Employee(AbstractUser):
+    hire_date = models.DateField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
