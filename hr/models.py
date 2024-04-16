605f6c7f4b3d5107497a69a85eb53dc6677f71f7
Kate Biliak <kate.bilyak@gmail.com>
2024-04-15 21:57:01 -0400

    1. "Company" model with fields: `name`, `address`, `email`, `tax_code` was added; 2. __str__ method which return name of company was added; 3. `phone_number` field was added to `Employee` model; 4. `job_description` field was added to `Position` model; 5. list_display was updated and return `phone_number` field to every employee

--- hr/models.py
+++ hr/models.py
@@ -5,7 +5,23 @@ from django.core.exceptions import ValidationError
 
 class Department(models.Model):
     name = models.CharField(max_length=200)
-    parent_department = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    parent_department = models.ForeignKey('self', on_delete=models.SET_NULL,
+                                          null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    tax_code = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Company.objects.exists():
+            raise ValidationError('There can be only one Company instance.')
+        return super(Company, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -15,12 +31,15 @@ class Position(models.Model):
     title = models.CharField(max_length=200)
     department = models.ForeignKey('Department', on_delete=models.CASCADE)
     is_manager = models.BooleanField(default=False)
+    job_description = models.CharField(max_length=200, default='N/A')
 
     def save(self, *args, **kwargs):
         if self.is_manager:
-            existing_manager = Position.objects.filter(department=self.department, is_manager=True).exists()
+            existing_manager = Position.objects.filter(
+                department=self.department, is_manager=True).exists()
             if existing_manager:
-                raise ValidationError(f"Manager already exists in the {self.department.name} department.")
+                raise ValidationError(
+                    f"Manager already exists in the {self.department.name} department.")
         super(Position, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -30,4 +49,6 @@ class Position(models.Model):
 class Employee(AbstractUser):
     hire_date = models.DateField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
+    position = models.ForeignKey('Position', on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
