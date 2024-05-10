ce0db90dd1c58a7c59e6ff4446ee2448327d4a29
jacob@appyventures.com <jacob@appyventures.com>
2024-05-10 21:31:44 +0300

    additional final

--- general/views.py
+++ general/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
-
+from hr.models import Company
 
 class HomeViev(TemplateView):
     template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = Company.objects.first()
+        context['company'] = Company
+        if Company and Company.logo:
+            context['logo_url'] = Company.logo
+        else:
+            context['logo_url'] = 'logo/'
+        return context
