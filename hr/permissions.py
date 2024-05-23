33787b3b9cc975d890e65483181dd2c933ba8194
oleh_donchenko <od.don.it@gmail.com>
2024-05-23 23:02:37 +0300

    Homework 11 - Розширення Функціоналу Django Rest Framework

--- hr/permissions.py
+++ hr/permissions.py
@@ -10,3 +10,8 @@ class IsNotRussianEmail(permissions.BasePermission):
         if request.user and request.user.email:
             return not request.user.email.endswith('.ru')
         return False
+
+
+class HasPositionPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.position is not None
