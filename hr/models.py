c61e83584c9a8715f570b41e8c9af0c29c0ca0c4
Revan190 <programmer23w@gmail.com>
2024-07-03 21:25:29 +0200

    Update models.py

--- hr/models.py
+++ hr/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
 from django.db import models
-
 
 class Department(models.Model):
     name = models.CharField(max_length=200)
@@ -12,7 +10,6 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
-
 class Position(models.Model):
     title = models.CharField(max_length=200)
     department = models.ForeignKey("Department", on_delete=models.CASCADE)
@@ -22,7 +19,8 @@ class Position(models.Model):
         if self.is_manager:
             existing_manager = Position.objects.filter(
                 department=self.department, is_manager=True
-            ).exists()
+            ).exclude(pk=self.pk).exists()  # Exclude the current instance
+
             if existing_manager:
                 raise ValidationError(
                     f"Manager already exists in the {self.department.name} department."
@@ -32,10 +30,16 @@ class Position(models.Model):
     def __str__(self):
         return self.title
 
-
 class Employee(AbstractUser):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    is_manager = models.BooleanField(default=False)
     hire_date = models.DateField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    id_number = models.CharField(max_length=200, null=True, blank=True)
     position = models.ForeignKey(
         "Position", on_delete=models.SET_NULL, null=True, blank=True
     )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
