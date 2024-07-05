44ecd3d5dc4e132de28f80e6ab5de73dae998739
Revan190 <programmer23w@gmail.com>
2024-07-05 14:28:15 +0200

    Edit views.py

--- hr/views.py
+++ hr/views.py
@@ -4,5 +4,12 @@ from .models import Department, Position
 def homework_querysets(request):
     # Example query: Get all departments
     departments = list(Department.objects.all().values())
+
+    # Example query: Get all positions
+    positions = list(Position.objects.all().values())
+
     # Convert query results to a list of dictionaries
-    return JsonResponse({'departments': departments})
+    return JsonResponse({
+        'departments': departments,
+        'positions': positions
+    })
