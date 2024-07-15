a9fe3ae93c61493fff6d8c0ff3979678dfac296f
Revan190 <programmer23w@gmail.com>
2024-07-15 12:20:01 +0200

    Delete rubbish

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Employee
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone_number', 'position')
-    # Add other configurations ...
