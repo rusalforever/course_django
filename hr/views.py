0ea7c95610a4cd103ab50c08f0ca82a6a21ecd26
Revan190 <programmer23w@gmail.com>
2024-07-15 12:05:00 +0200

    Edit mistakes

--- hr/views.py
+++ hr/views.py
@@ -1,21 +1,20 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from .models import Department, Position
 
 def homework_querysets(request):
-    # Query 1: All departments
+    # Запит 1
     departments = Department.objects.all()
 
-    # Query 2: All positions
+    # Запит 2:
     positions = Position.objects.all()
 
-    # Query 3: Active positions
+    # Запит 3:
     active_positions = Position.objects.filter(is_active=True)
 
-    # Query 4: Manager positions
+    # Запит 4:
     manager_positions = Position.objects.filter(is_manager=True)
 
-    # Query 5: Positions in a specific department (e.g., department with id=1)
+    # Запит 5:
     specific_department_positions = Position.objects.filter(department_id=1)
 
     context = {
@@ -26,20 +25,3 @@ def homework_querysets(request):
         'specific_department_positions': specific_department_positions,
     }
     return render(request, 'homework_querysets.html', context)
-=======
-from django.http import JsonResponse
-from .models import Department, Position
-
-def homework_querysets(request):
-    # Example query: Get all departments
-    departments = list(Department.objects.all().values())
-
-    # Example query: Get all positions
-    positions = list(Position.objects.all().values())
-
-    # Convert query results to a list of dictionaries
-    return JsonResponse({
-        'departments': departments,
-        'positions': positions
-    })
->>>>>>> baa3c4a14e311c91efca999660cd21b362ae316c
