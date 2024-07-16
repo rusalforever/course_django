813cc9d04606b7cdaa4e22184b8656944992e8db
Revan190 <programmer23w@gmail.com>
2024-07-16 12:58:59 +0200

    Edit settings

--- course_django/settings.py
+++ course_django/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'modeltranslation',
+    'modeltranslation',  # Add this line for modeltranslation
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
