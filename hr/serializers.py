09bcad2e861fa62971d792fdaecec0212a62b24d
Revan190 <programmer23w@gmail.com>
2024-07-16 14:51:15 +0200

    Homework_part2

--- hr/serializers.py
+++ hr/serializers.py
@@ -1,22 +1,19 @@
 from rest_framework import serializers
-
 from hr.models import (
     Employee,
     Position,
+    Department,  # Add this import
 )
-
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = ('id', 'username', 'first_name', 'last_name', 'email', 'position')
 
-
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
         fields = ('id', 'title', 'department', 'is_manager', 'is_active', 'job_description', 'monthly_rate')
-
 
 class SalarySerializer(serializers.Serializer):
     employee = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
@@ -25,3 +22,8 @@ class SalarySerializer(serializers.Serializer):
     sick_days = serializers.IntegerField(default=0)
     vacation_days = serializers.IntegerField(default=0)
 
+# Add the DepartmentSerializer class
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
