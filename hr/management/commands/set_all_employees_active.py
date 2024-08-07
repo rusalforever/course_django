f567cc8d9d3a7ca245cb5d724428cf5dd0bc00b9
Kate Biliak <kate.bilyak@gmail.com>
2024-08-07 17:17:10 -0400

    Optimized updating employees

--- hr/management/commands/set_all_employees_active.py
+++ hr/management/commands/set_all_employees_active.py
@@ -1,16 +1,9 @@
 from django.core.management.base import BaseCommand
-from django.utils import timezone
-
 from hr.models import Employee
 
 
 class Command(BaseCommand):
-    help = 'Set "paid" as True for all MonthlySalary instances where month_year is the current month'  # noqa: A003
+    help = 'Set "paid" as True for all employees'  # noqa: A003
 
     def handle(self, *args, **kwargs):
-        employees = Employee.objects.all()
-        for employee in employees:
-            if not employee.is_active:
-                employee.is_active = True
-                employee.save()
-                print(f"employee {employee.id} / {employee.username} set to {employee.is_active}")
+        Employee.objects.filter(is_active=False).update(is_active=True)
